fc78de16c0de88d4ddbe1bad56190cf887c0c347
Dennis Njore <dknjore@gmail.com>
2024-11-01 07:24:37 +0000

    my_list update file

---
+++ my_list.py
@@ -0,0 +1,16 @@
+my_list = []
+
+my_list.extend([10, 20, 30, 40])
+
+my_list.insert(1, 15)
+
+my_list.extend([50, 60, 70])
+
+my_list.pop()
+
+my_list.sort()
+
+index_of_30 = my_list.index(30)
+
+print("Final list:", my_list)
+print("Index of 30:", index_of_30)
--- user_input.py
+++ user_input.py
@@ -4,3 +4,4 @@ age = input("How old are you? ")
 location = input("Where do you live? ")
 
 print(f"Hello {name}, you are {age} years old and live in {location}.")
+
